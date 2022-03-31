9e5796166dbbdb0dc793deb25274004a3cbe1138
Anjali kumari <anjalikumari01051998@gmail.com>
2022-03-31 12:48:31 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { Navbar, Sidebar, Footer } from './components'
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ProductsPage from './pages/ProductsPage';
+import ErrorPage from './pages/ErrorPage';
+import CheckoutPage from './pages/CheckoutPage';
+import SingleProductPage from './pages/SingleProductPage';
+import CartPage from './pages/CartPage';
+import PrivateRoute from './pages/PrivateRoute'
+import  AuthWrapper  from './pages/AuthWrapper';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <>
+  <AuthWrapper>
+  <Router>
+    <Navbar/>
+    <Sidebar/>
+    <Switch>
+      <Route exact path='/'><HomePage/></Route>
+      <Route exact path='/about'><AboutPage/></Route>
+      <Route exact path='/products'><ProductsPage/></Route>
+      <Route exact path='/products/:id' children={<SingleProductPage/>}></Route>
+      <PrivateRoute exact path='/checkout'><CheckoutPage/></PrivateRoute>
+      <Route exact path='/cart'><CartPage/></Route>
+      <Route path='*'><ErrorPage/></Route>
+    </Switch>
+    <Footer/>
+  </Router>
+  </AuthWrapper>
+
+  </>
 }
 
-export default App;
+export default App
